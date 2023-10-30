03646b4b1ffb804582fb40a52f7d337b27768711
tahirasif31 <tahirasif31@gmail.com>
2023-10-30 08:05:31 -0700

    tip calculator completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { Children, useState } from "react";
 
 function App() {
+  const [rating, setRating] = useState("");
+  const [friendRating, setFriendRating] = useState("");
+  const [bill, setBill] = useState("");
+
+  const needReset = rating || bill || friendRating;
+
+  let average;
+
+  function calculateTipAverage() {
+    average = (rating + friendRating) / 2;
+    console.log(average);
+  }
+
+  calculateTipAverage();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <BillInput bill={bill} onAddBill={setBill} />
+        <ServiceRatingInput rating={rating} onAddRating={setRating}>
+          How did you like the service?
+        </ServiceRatingInput>
+        <ServiceRatingInput rating={friendRating} onAddRating={setFriendRating}>
+          How did your friend like the service?
+        </ServiceRatingInput>
+        <BillCalculator bill={bill} average={average} />
+        {needReset && (
+          <Reset
+            onAddBill={setBill}
+            onAddRating={setRating}
+            onAddFriendRating={setFriendRating}
+          />
+        )}
+      </div>
     </div>
+  );
+}
+
+function BillInput({ bill, onAddBill }) {
+  return (
+    <div className="bill-input">
+      <p>How much was the bill?</p>
+      <input
+        type="text"
+        className="input"
+        value={bill}
+        onChange={(e) => onAddBill(Number(e.target.value))}
+      />
+    </div>
+  );
+}
+
+function ServiceRatingInput({ children, rating, onAddRating }) {
+  return (
+    <div className="service-rating-input">
+      <p>{children}</p>
+      <select
+        value={rating}
+        onChange={(e) => onAddRating(Number(e.target.value))}
+        className="service"
+      >
+        <option value={0}>Dissatisfied (0%)</option>
+        <option value={5}>It was okay (5%)</option>
+        <option value={10}>It was good (10%)</option>
+        <option value={20}>Absolutely amazing (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function BillCalculator({ bill, average }) {
+  return (
+    <div className="final-bill">{`Your bill is $${
+      bill + average
+    } ($${bill} + $${average})`}</div>
+  );
+}
+
+function Reset({ onAddBill, onAddRating, onAddFriendRating }) {
+  function resetAll() {
+    onAddBill("");
+    onAddRating("");
+    onAddFriendRating("");
+  }
+
+  return (
+    <button onClick={resetAll} className="btn">
+      Reset
+    </button>
   );
 }
 
